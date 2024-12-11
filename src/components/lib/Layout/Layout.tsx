9a0d9faaea26d5b2359bca8f77abea88e13a4940
amir hossein khakbaz <amir73khakbaz@gmail.com>
2024-12-11 16:23:25 +0330

    add link to login

--- src/components/lib/Layout/Layout.tsx
+++ src/components/lib/Layout/Layout.tsx
@@ -1,11 +1,21 @@
 import { Button } from "@chakra-ui/react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Layout = () => {
   return (
     <div>
-      Layout
-      <Button>Click me</Button>
+      <header
+        style={{
+          backgroundColor: "#f2f5f9",
+          width: "100%",
+          height: "60px",
+        }}
+      >
+        <Link to="/login">
+          <Button colorPalette="green">Login</Button>
+        </Link>
+      </header>
     </div>
   );
 };
