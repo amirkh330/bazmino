7f3d88e49f55861f4737f237ddfded71486362cb
amirkh330 <amir73khakbaz@gmail.com>
2024-12-26 13:48:01 +0330

    add overlay on bottom sheet

--- src/components/CoreComponnets/BottomSheet/BottomSheet.tsx
+++ src/components/CoreComponnets/BottomSheet/BottomSheet.tsx
@@ -1,10 +1,4 @@
-import {
-  Box,
-  CloseButton,
-  Flex,
-  Slide,
-  Text
-} from "@chakra-ui/react";
+import { Box, CloseButton, Flex, Slide, Text } from "@chakra-ui/react";
 
 interface IBottomSheet {
   title: string;
@@ -14,25 +8,38 @@ interface IBottomSheet {
   onClose: () => void;
 }
 const BottomSheet = ({ title, children, isOpen, onClose }: IBottomSheet) => {
+  if (!isOpen) return null;
   return (
-    <Box textAlign="center" p="6" maxW={"400px"}>
-      <Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}>
-        <Box bg="white" p="6" boxShadow="md" borderTopRadius="md">
-          <Box height="5px" bg="gray.300" borderRadius="full" mb="4" />
-          <Flex
-            justifyContent="space-between"
-            alignItems="center"
-            mb="4"
-            borderBottom={"1px solid"}
-            pb="4"
-          >
-            <Text>{title}</Text>
-            <CloseButton onClick={onClose}/>
-          </Flex>
-          <Box>{children}</Box>
-        </Box>
-      </Slide>
-    </Box>
+    <>
+      <Box textAlign="center" p="6" maxW={"400px"}>
+        <Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}>
+          <Box bg="white" p="6" boxShadow="md" borderTopRadius="md">
+            <Box height="5px" bg="gray.300" borderRadius="full" mb="4" />
+            <Flex
+              justifyContent="space-between"
+              alignItems="center"
+              mb="4"
+              borderBottom={"1px solid"}
+              pb="4"
+            >
+              <Text>{title}</Text>
+              <CloseButton onClick={onClose} />
+            </Flex>
+            <Box>{children}</Box>
+          </Box>
+        </Slide>
+      </Box>
+      <Box
+        onClick={onClose}
+        position="fixed"
+        top="0"
+        right="0"
+        bottom="0"
+        left="0"
+        bg="blackAlpha.600"
+        backdropFilter="blur(10px)"
+      ></Box>
+    </>
   );
 };
 
