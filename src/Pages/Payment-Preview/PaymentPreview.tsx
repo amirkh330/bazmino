2844d20d742e8667e0cddda6438169c45fe2e971
amirkh330 <amir73khakbaz@gmail.com>
2025-01-03 14:29:34 +0330

    fix style

--- src/Pages/Payment-Preview/PaymentPreview.tsx
+++ src/Pages/Payment-Preview/PaymentPreview.tsx
@@ -5,9 +5,12 @@ import {
   chakra,
   Divider,
   Flex,
+  Icon,
   Input,
   Text,
 } from "@chakra-ui/react";
+import { CheckCircle } from "@phosphor-icons/react";
+import { Cardholder } from "@phosphor-icons/react/dist/ssr";
 import { useParams } from "react-router-dom";
 export const PaymentPreview = () => {
   const { id } = useParams();
@@ -19,68 +22,90 @@ export const PaymentPreview = () => {
   };
   return (
     <chakra.div>
-      <Flex flexDirection="column" mx="0" px="4" gap="24px">
-        <Text fontSize={"16px"} fontWeight={600} color={"amir.common"}>
-          جزئیات رزرو بازی
-        </Text>
-        <Flex mx="0" alignItems="center" justifyContent="space-between">
-          <Text fontSize={"14px"} fontWeight={400} color={"amir.common"}>
-            هزینه رزرو (۴ نفر)
+      <chakra.div h="calc(100vh - 110px)">
+        <Flex flexDirection="column" mx="0" px="4" gap="24px" pt="8px">
+          <Text fontSize={"16px"} fontWeight={600} color={"amir.common"}>
+            جزئیات رزرو بازی
           </Text>
-          <Text fontSize={"14px"} fontWeight={400} color={"amir.common"}>
-            ۶۲۹.۰۰۰ تومان
-          </Text>
-        </Flex>
-        <Flex mx="0" alignItems="center" justifyContent="space-between">
-          <Text fontSize={"14px"} fontWeight={400} color={"amir.common"}>
-            تخفیف{" "}
-          </Text>
-          <Text fontSize={"14px"} fontWeight={400} color={"amir.common"}>
-            ۶۲۹.۰۰۰ تومان
-          </Text>
-        </Flex>
-      </Flex>
-      <Divider color="gray.600" my="24px" />
-      <Flex flexDirection="column" mx="0" px="4" gap="24px">
-        <Text fontSize={"16px"} fontWeight={600} color={"amir.common"}>
-          کد تخفیف
-        </Text>
-        <Flex mx="0" alignItems="center" justifyContent="space-between" gap="2">
-          <Input color="amir.common" />
-          <Button
-            variant={"outline"}
-            borderColor={"amir.primary"}
-            color={"amir.primary"}
-          >
-            اعمال کد
-          </Button>
-        </Flex>
-      </Flex>
-      <Divider color="gray.600" my="24px" />
-      <Flex flexDirection="column" mx="0" px="4" gap="24px">
-        <Text fontSize={"16px"} fontWeight={600} color={"amir.common"}>
-          انتخاب روش پرداخت
-        </Text>
-
-        <Flex
-          borderRadius="8px"
-          border="1px solid"
-          borderColor={"amir.primary"}
-          background="#414141"
-          px="16px"
-          py="16px"
-          mx="0"
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <Flex alignItems="center" mx="0">
+          <Flex mx="0" alignItems="center" justifyContent="space-between">
             <Text fontSize={"14px"} fontWeight={400} color={"amir.common"}>
-              پرداخت مستقیم از درگاه
+              هزینه رزرو (۴ نفر)
+            </Text>
+            <Text fontSize={"14px"} fontWeight={400} color={"amir.common"}>
+              ۶۲۹.۰۰۰ تومان
+            </Text>
+          </Flex>
+          <Flex mx="0" alignItems="center" justifyContent="space-between">
+            <Text fontSize={"14px"} fontWeight={400} color={"amir.common"}>
+              تخفیف{" "}
+            </Text>
+            <Text fontSize={"14px"} fontWeight={400} color={"amir.common"}>
+              ۶۲۹.۰۰۰ تومان
             </Text>
           </Flex>
         </Flex>
+        <Divider color="gray.600" my="24px" />
+        <Flex flexDirection="column" mx="0" px="4" gap="24px">
+          <Text fontSize={"16px"} fontWeight={600} color={"amir.common"}>
+            کد تخفیف
+          </Text>
+          <Flex
+            mx="0"
+            alignItems="center"
+            justifyContent="space-between"
+            gap="2"
+          >
+            <Input color="amir.common" />
+            <Button
+              variant={"outline"}
+              borderColor={"amir.primary"}
+              color={"amir.primary"}
+            >
+              اعمال کد
+            </Button>
+          </Flex>
+        </Flex>
+        <Divider color="gray.600" my="24px" />
+        <Flex flexDirection="column" mx="0" px="4" gap="24px">
+          <Text fontSize={"16px"} fontWeight={600} color={"amir.common"}>
+            انتخاب روش پرداخت
+          </Text>
 
-
+          <Flex
+            borderRadius="8px"
+            border="1px solid"
+            borderColor={"amir.primary"}
+            background="#414141"
+            px="16px"
+            py="16px"
+            mx="0"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Flex alignItems="center" mx="0" gap="2">
+              <Icon
+                as={Cardholder}
+                mx="0"
+                w="6"
+                h="6"
+                p="1"
+                bgColor={"amir.common"}
+                borderRadius={"50%"}
+              />
+              <Text fontSize={"14px"} fontWeight={400} color={"amir.common"}>
+                پرداخت مستقیم از درگاه
+              </Text>
+            </Flex>
+            <Icon
+              as={CheckCircle}
+              mx="0"
+              color={"amir.primary"}
+              weight="fill"
+            />
+          </Flex>
+        </Flex>
+      </chakra.div>
+      <chakra.div px="4">
         <Button
           width={"100%"}
           variant={"solid"}
@@ -90,7 +115,7 @@ export const PaymentPreview = () => {
         >
           پرداخت
         </Button>
-      </Flex>
+      </chakra.div>
     </chakra.div>
   );
 };
