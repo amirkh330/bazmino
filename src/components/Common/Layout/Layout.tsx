08d2fade9901ef563749d633e1a4bba7e4a46cf2
amir hossein khakbaz <amir73khakbaz@gmail.com>
2025-01-06 09:33:57 +0330

    add logic after get 401

--- src/components/Common/Layout/Layout.tsx
+++ src/components/Common/Layout/Layout.tsx
@@ -1,14 +1,17 @@
 import { CallApi } from "@/settings/axiosConfig";
-import { Box, useToast } from "@chakra-ui/react";
+import { Box, useDisclosure, useToast } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
 import useAuthStore from "@/store/authStore";
+import { Login } from "../Login/Login";
 
 export const Layout = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { logout } = useAuthStore();
   const navigate = useNavigate();
+
   const toast = useToast();
   useEffect(() => {
     CallApi.get(`/me`)
@@ -17,12 +20,13 @@ export const Layout = () => {
         if (status == 401) {
           logout();
           navigate("/");
-          toast({
-            description: "لطفا وارد شوید",
-            status: "info",
-            duration: 2000,
-            position:"top"
-          });
+          onOpen();
+          // toast({
+          //   description: "لطفا وارد شوید",
+          //   status: "info",
+          //   duration: 2000,
+          //   position: "top",
+          // });
         }
       });
   }, []);
@@ -31,6 +35,8 @@ export const Layout = () => {
       <Header />
       {/* <Box mx="auto" height="calc(100dvh - 56px)"> */}
       <Outlet />
+      {isOpen && <Login  isOpen={isOpen} onOpen={onOpen} onClose={onClose} />}
+
       {/* </Box> */}
     </Box>
   );
