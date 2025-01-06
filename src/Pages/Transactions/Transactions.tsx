c7d5af560f94c86996597735f86457c6f5be80fe
amirkh330 <amir73khakbaz@gmail.com>
2025-01-06 23:53:51 +0330

    add wallet and transactions

--- src/Pages/Transactions/Transactions.tsx
+++ src/Pages/Transactions/Transactions.tsx
@@ -9,17 +9,64 @@ import {
   Icon,
   Text,
 } from "@chakra-ui/react";
+import { Toman } from "@/utils/Toman/Toman";
 export const Transactions = () => {
   return (
     <chakra.div
-      pt="4"
       h="calc(100dvh - 56px)"
       display="flex"
       flexDirection="column"
       m="0"
       justifyContent="space-between"
     >
-      <chakra.div m="0" overflow="auto" mb="4"></chakra.div>
+      <chakra.div m="0" overflow="auto" mb="4" px="4">
+        {Array(10)
+          .fill(0)
+          .map((_, index) => (
+            <Flex
+              key={index}
+              alignItems="center"
+              justifyContent="space-between"
+              p="2"
+              my="4"
+              borderRadius="8px"
+              bg="amir.secondaryBg"
+            >
+              <Box mx="0">
+                <Text fontSize={"14px"} fontWeight={500} color="amir.common">
+                  برداشت از کیف پول
+                </Text>
+                <Text
+                  my="1"
+                  fontSize={"14px"}
+                  fontWeight={500}
+                  color="amir.common"
+                >
+                  سه‌شنبه ۱۲ بهمن
+                </Text>
+              </Box>
+              <Box mx="0" textAlign={"center"}>
+                <Text fontSize={"14px"} fontWeight={500} color="amir.common">
+                  {Toman(120000)}
+                </Text>
+                <Text
+                  my="1"
+                  mx="auto"
+                  borderRadius="10px"
+                  textAlign={"center"}
+                  p={"4px"}
+                  w={"fit-content"}
+                  bg="green.200"
+                  fontWeight={400}
+                  fontSize={"12px"}
+                  color="green.700"
+                >
+                  موفق
+                </Text>
+              </Box>
+            </Flex>
+          ))}
+      </chakra.div>
       <Footer />
     </chakra.div>
   );
--- src/Pages/Wallet/Wallet.tsx
+++ src/Pages/Wallet/Wallet.tsx
@@ -1,27 +1,57 @@
-
-import React from 'react'
+import React from "react";
 import { Footer } from "@/components/Common/Footer/Footer";
 import {
   Avatar,
   Box,
+  Button,
   chakra,
   Divider,
   Flex,
   Icon,
+  Image,
   Text,
 } from "@chakra-ui/react";
+import { Toman } from "@/utils/Toman/Toman";
 export const Wallet = () => {
   return (
     <chakra.div
-          pt="4"
-          h="calc(100dvh - 56px)"
-          display="flex"
-          flexDirection="column"
-          m="0"
-          justifyContent="space-between"
+      pt="4"
+      h="calc(100dvh - 56px)"
+      display="flex"
+      flexDirection="column"
+      m="0"
+      justifyContent="space-between"
+    >
+      <chakra.div m="0" overflow="auto" mb="4" my="4" mx="auto">
+        <Image src="/images/wallet.svg" />
+        <Text
+          my="4"
+          textAlign="center"
+          fontSize={"14px"}
+          fontWeight={400}
+          color={"amir.common"}
         >
-          <chakra.div m="0" overflow="auto" mb="4"></chakra.div>
-          <Footer />
-        </chakra.div>
-  )
-}
+          موجودی کیف پول:
+        </Text>
+        <Text
+          textAlign="center"
+          fontSize={"16px"}
+          fontWeight={600}
+          color={"amir.common"}
+        >
+          {Toman(2222200)}
+        </Text>
+      </chakra.div>
+      <chakra.div px="4" my="4" mx="0">
+        <Button
+          w="full"
+          variant="outline"
+          borderColor="amir.primary"
+          color="amir.primary"
+        >
+          برداشت موجودی
+        </Button>
+      </chakra.div>
+    </chakra.div>
+  );
+};
